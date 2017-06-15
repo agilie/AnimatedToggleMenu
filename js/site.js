63c800a725121394d50e24e97f20e7e9e815c0a8
kushnir89 <tatiana.kushnir@agilie.com>
2017-06-15 20:10:58 +0300

    first commit

---
+++ js/site.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+    $('.hamburger-menu').click(function (e) {
+        e.preventDefault();
+        if ($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $('.menu-overlay').fadeToggle( 'fast', 'linear' );
+            $('.menu .menu-list').slideToggle( 'slow', 'swing' )
+        } else {
+            $(this).addClass('active');
+            $('.menu-overlay').fadeToggle( 'fast', 'linear' );
+            $('.menu .menu-list').slideToggle( 'slow', 'swing' );
+            $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
+        }
+    })
+})();
